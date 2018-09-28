0c132fa425573484d01d750d7e2dc0b46d32c02c
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2018-09-28 13:51:28 -0500

    Created hacky uninstall

--- servicecontrol/tools/chocolateyuninstall.ps1
+++ servicecontrol/tools/chocolateyuninstall.ps1
@@ -1,37 +1,15 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
 
-## NOTE: In 80-90% of the cases (95% with licensed versions due to Package Synchronizer and other enhancements),
-## AutoUninstaller should be able to detect and handle registry uninstalls without a chocolateyUninstall.ps1.
-## See https://chocolatey.org/docs/commands-uninstall
-## and https://chocolatey.org/docs/helpers-uninstall-chocolatey-package
+$ErrorActionPreference = 'Stop'
 
-## If this is an MSI, ensure 'softwareName' is appropriate, then clean up comments and you are done.
-## If this is an exe, change fileType, silentArgs, and validExitCodes
-
-$ErrorActionPreference = 'Stop'; # stop on all errors
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = 'servicecontrol*'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  fileType      = 'EXE_MSI_OR_MSU' #only one of these: MSI or EXE (ignore MSU for now)
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'ServiceControl'
+  # Installer fails when fileType is not EXE and uninstall fails when fileType is not MSI.  It's confusing but it's the only way it works.
+  # It's most likely because the below conditional in this file that deals with looking for certain keys in the registry if the file is an MSI
+  fileType       = 'MSI'
   # MSI
-  silentArgs    = "/qn /norestart"
-  validExitCodes= @(0, 3010, 1605, 1614, 1641) # https://msdn.microsoft.com/en-us/library/aa376931(v=vs.85).aspx
-  # OTHERS
-  # Uncomment matching EXE type (sorted by most to least common)
-  #silentArgs   = '/S'           # NSIS
-  #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
-  #silentArgs   = '/s'           # InstallShield
-  #silentArgs   = '/s /v"/qn"'   # InstallShield with MSI
-  #silentArgs   = '/s'           # Wise InstallMaster
-  #silentArgs   = '-s'           # Squirrel
-  #silentArgs   = '-q'           # Install4j
-  #silentArgs   = '-s -u'        # Ghost
-  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
-  #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
-                                 #       https://chocolatey.org/packages/autohotkey.portable
-  #validExitCodes= @(0) #please insert other valid exit codes here
+  silentArgs     = "/quiet /norestart /LV* `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).log`""
+  validExitCodes = @(0)
 }
 
 $uninstalled = $false
@@ -67,11 +45,3 @@ if ($key.Count -eq 1) {
   Write-Warning "Please alert package maintainer the following keys were matched:"
   $key | % {Write-Warning "- $($_.DisplayName)"}
 }
-
-## OTHER POWERSHELL FUNCTIONS
-## https://chocolatey.org/docs/helpers-reference
-#Uninstall-ChocolateyZipPackage $packageName # Only necessary if you did not unpack to package directory - see https://chocolatey.org/docs/helpers-uninstall-chocolatey-zip-package
-#Uninstall-ChocolateyEnvironmentVariable # 0.9.10+ - https://chocolatey.org/docs/helpers-uninstall-chocolatey-environment-variable 
-#Uninstall-BinFile # Only needed if you used Install-BinFile - see https://chocolatey.org/docs/helpers-uninstall-bin-file
-## Remove any shortcuts you added in the install script.
-
