165c9562b1d09b4e28772066f5dfcf5c9e61620a
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2018-10-05 18:48:19 -0500

    Update to latest version

--- servicecontrol/tools/chocolateyinstall.ps1
+++ servicecontrol/tools/chocolateyinstall.ps1
@@ -3,14 +3,14 @@
 $ErrorActionPreference = 'Stop'
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/Particular/ServiceControl/releases/download/3.1.2/Particular.ServiceControl-3.1.2.exe'
+$url64      = 'https://github.com/Particular/ServiceControl/releases/download/3.2.1/Particular.ServiceControl-3.2.1.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
   url64bit       = $url64
   softwareName   = 'Particular Software ServiceControl Management'
-  checksum64     = '26E3B9E75057F216B274FA4FD958A480544F74EA5464D938081442CCCBBAEECA'
+  checksum64     = 'C41FF76A0707BA1F727CA5411A1774DB762C88120FA5CE3FE685A8E53065E41E'
   checksumType64 = 'sha256'
   silentArgs     = "/quiet /norestart /LV* `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).log`""
   validExitCodes = @(0)
