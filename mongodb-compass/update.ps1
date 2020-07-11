46549f387c67cee1a7bb6e0cecc45ebf05750b6c
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2020-07-11 02:31:12 -0500

    Added mongodb compass

--- mongodb-compass/update.ps1
+++ mongodb-compass/update.ps1
@@ -14,19 +14,19 @@ function global:au_SearchReplace {
 # Get latest version + download url of the software
 function global:au_GetLatest {
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $latest_release = Invoke-RestMethod -Method Get -Uri 'https://api.github.com/repos/mongodb-js/mongosh/releases/latest'
+    $latest_release = Invoke-RestMethod -Method Get -Uri 'https://api.github.com/repos/mongodb-js/compass/releases/latest'
         
     foreach( $asset in $latest_release.assets) 
     {
-        if ($asset.browser_download_url.Contains("win32-zip"))
+        if ($asset.browser_download_url.Contains("win32-x64.msi"))
         {
-            $zip_download_url = $asset.browser_download_url
+            $msi_download_url = $asset.browser_download_url
         }
     }
 
     @{
         Version = $latest_release.tag_name
-        URL64   = $zip_download_url
+        URL64   = $msi_download_url
     }
 
 }
