efba529fd3f9c24b777978566ccbb7f3e82513f9
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2020-07-12 21:47:38 -0500

    Added mongodb database tools (#16)

---
+++ mongodb-database-tools/update.ps1
@@ -0,0 +1,26 @@
+Import-Module AU
+
+
+# Find and replace content basd on latest version found
+function global:au_SearchReplace {
+    @{
+        '.\tools\chocolateyinstall.ps1' = @{
+            "(^[$]url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+# Get latest version + download url of the software
+function global:au_GetLatest {
+    $response = Invoke-RestMethod -Method Get -Uri "https://downloads.mongodb.org/tools/db/release.json"
+    $windows_zip_archive = $response.versions.downloads.archive.Where({ $PSItem.url -like "*.zip"})
+
+    @{
+        Version = $response.versions.version
+        URL64   = $windows_zip_archive
+    }
+
+}
+
+Update-Package -ChecksumFor 64
