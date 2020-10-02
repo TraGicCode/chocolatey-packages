d9c0119b7f9b7a20c78ae56213a145bd381bd09e
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2020-10-01 23:04:57 -0500

    (GH-21) Create MongoDB Compass Readonly Package (#27)
    Closes #21

---
+++ mongodb-compass-readonly/update.ps1
@@ -0,0 +1,34 @@
+Import-Module AU
+
+
+# Find and replace content basd on latest version found
+function global:au_SearchReplace {
+    @{
+        '.\tools\chocolateyinstall.ps1' = @{
+            "(^[$]url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+# Get latest version + download url of the software
+function global:au_GetLatest {
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $latest_release = Invoke-RestMethod -Method Get -Uri 'https://api.github.com/repos/mongodb-js/compass/releases/latest'
+    $version = $latest_release.name
+    foreach( $asset in $latest_release.assets) 
+    {
+        if ($asset.browser_download_url.Contains("mongodb-compass-readonly-$version-win32-x64.msi"))
+        {
+            $msi_download_url = $asset.browser_download_url
+        }
+    }
+
+    @{
+        Version = $version
+        URL64   = $msi_download_url
+    }
+
+}
+
+Update-Package -ChecksumFor 64
