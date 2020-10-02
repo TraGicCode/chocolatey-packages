cd24bfa496853cb179ebe3e133e5d3a28ea6094e
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2020-10-01 22:53:54 -0500

    (GH-24) Get full version of compass url (#25)
    Closes #24

--- mongodb-compass/update.ps1
+++ mongodb-compass/update.ps1
@@ -15,17 +15,17 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     $latest_release = Invoke-RestMethod -Method Get -Uri 'https://api.github.com/repos/mongodb-js/compass/releases/latest'
-        
+    $version = $latest_release.name
     foreach( $asset in $latest_release.assets) 
     {
-        if ($asset.browser_download_url.Contains("win32-x64.msi"))
+        if ($asset.browser_download_url.Contains("mongodb-compass-$version-win32-x64.msi"))
         {
             $msi_download_url = $asset.browser_download_url
         }
     }
 
     @{
-        Version = $latest_release.tag_name.Substring(1)
+        Version = $version
         URL64   = $msi_download_url
     }
 
