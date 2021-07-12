52fb1cee080f470a4859ee44696408d48fe54e9f
michaelf <6224270+TraGicCode@users.noreply.github.com>
2021-07-11 23:14:40 -0500

    Add ILRepack chocolatey package

---
+++ il-repack/update.ps1
@@ -0,0 +1,24 @@
+Import-Module AU
+
+
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -Purge -NoSuffix
+
+    set-alias 7z $Env:chocolateyInstall\tools\7z.exe
+    rm tools\*.exe
+    7z e tools\*.nupkg NuGet.exe -r -otools
+    rm tools\*.nupkg
+}
+
+function global:au_GetLatest {
+    $packageName = "ILRepack"
+    $package = Find-Package $packageName -provider "nuget" -Source http://www.nuget.org/api/v2/ -Force
+    $version = $package.Version
+    @{
+        Version = $version
+        Url32   = "https://api.nuget.org/packages/$($packageName.ToLower()).${version}.nupkg"
+    }
+}
+
+
+Update-Package -ChecksumFor 32
