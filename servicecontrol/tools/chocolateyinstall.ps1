accea179be33edd06c0ff5a4b0b4a6b709db4b68
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2023-12-08 09:05:11 -0600

    Convert packages to portable package since v5 breaking changes (#46)

--- servicecontrol/tools/chocolateyinstall.ps1
+++ servicecontrol/tools/chocolateyinstall.ps1
@@ -5,15 +5,14 @@ $ErrorActionPreference = 'Stop'
 $url64bit   = 'https://github.com/Particular/ServiceControl/releases/download/5.0.0/Particular.ServiceControl-5.0.0.exe'
 $checksum64 = '081e3c2ca729b96f9d5ff9f51a7f00fd6fe505d37e1c3ab23e898aa409c09142'
 
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  fileType       = 'EXE'
-  softwareName   = 'Particular Software ServiceControl Management'
   url64bit       = $url64bit
   checksum64     = $checksum64
   checksumType64 = 'sha256'
-  silentArgs     = "/quiet /norestart /LV* `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).log`""
-  validExitCodes = @(0)
+  fileFullPath   = "$toolsDir\Particular.ServiceControl.exe"
 }
 
-Install-ChocolateyPackage @packageArgs
+Get-ChocolateyWebFile @packageArgs
