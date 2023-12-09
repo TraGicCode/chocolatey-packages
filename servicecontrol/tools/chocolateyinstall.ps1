012a8e0a4a734180bf73857403c4cd8381d6ef6c
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2023-12-09 08:24:34 -0600

    Chore/more updates to servicecontrol package (#48)
    * WIP
    
    * WIP

--- servicecontrol/tools/chocolateyinstall.ps1
+++ servicecontrol/tools/chocolateyinstall.ps1
@@ -1,11 +1,9 @@
-﻿# https://docs.particular.net/servicecontrol/installation-silent
+﻿$ErrorActionPreference = 'Stop'
 
-$ErrorActionPreference = 'Stop'
-
-$url64bit   = 'https://github.com/Particular/ServiceControl/releases/download/5.0.1/Particular.ServiceControl-5.0.1.exe'
-$checksum64 = '233a6d3a6547f48f8151463838bd10a46b2d1cbffc660cb7a69c9d299de00761'
-
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64bit      = 'https://github.com/Particular/ServiceControl/releases/download/5.0.1/Particular.ServiceControl-5.0.1.exe'
+$checksum64    = '233a6d3a6547f48f8151463838bd10a46b2d1cbffc660cb7a69c9d299de00761'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$pp = Get-PackageParameters
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -16,3 +14,16 @@ $packageArgs = @{
 }
 
 Get-ChocolateyWebFile @packageArgs
+
+if (!$pp.NoDesktop) {
+  Write-Host "Creating desktop shortcut for ServiceControl..."
+  # Create desktop shortcut
+  $desktop = [System.Environment]::GetFolderPath('CommonDesktopDirectory')
+  Install-ChocolateyShortcut -ShortcutFilePath "$desktop\ServiceControl Management.lnk" -TargetPath $packageArgs.fileFullPath
+}
+
+if (!$pp.NoStartMenu) {
+  Write-Host "Creating Start Menu shortcut for ServiceControl..."
+  $startMenu = [System.Environment]::GetFolderPath('CommonStartMenu')
+  Install-ChocolateyShortcut -ShortcutFilePath "$startMenu\ServiceControl Management.lnk" -TargetPath $packageArgs.fileFullPath
+}
