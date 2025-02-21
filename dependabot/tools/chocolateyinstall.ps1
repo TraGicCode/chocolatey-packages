def0bf23589d877022b20ee2a8764c6074d1ac77
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2025-02-20 22:23:08 -0600

    Add dependabot cli (#52)

---
+++ dependabot/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$url64bit   = 'https://github.com/dependabot/cli/releases/download/v1.59.0/dependabot-v1.59.0-windows-amd64.zip'
+$checksum64 = '13F33F94A9B5E9D82D135A16FDA78E467F5B4E1F526F89DEEE07A7A7937617B8'
+
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  url64bit       = $url64bit
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
